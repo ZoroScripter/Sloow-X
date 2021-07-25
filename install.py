51ab4ab43e17147871b5c328a8580bc83e12a04f
ZoroScripter <87605900+ZoroScripter@users.noreply.github.com>
2021-07-25 11:45:38 -0300

    install

---
+++ install.py
@@ -0,0 +1,248 @@
+###################
+# Version 1.0     #
+###################
+import os
+import time
+
+#Cores do python que irei usar:
+#\033[1;30m Branco
+#\033[1;31m Vermelho
+#\033[1;32m Verde
+#\033[1;33m Amarelo
+#\033[1;34m Azul
+#\033[1;35m Roxo
+#\033[1;36m Ciano
+#\033[1;37m Cinza Claro
+
+os.system("clear")
+os.system("pkg install figlet")
+os.system("clear")
+#Começando o Script!!
+time.sleep(3)
+print("\033[1;33m")
+os.system("figlet Sloow-X")
+time.sleep(3)
+print("""\033[1;35m
+============================
+l=l 1 - Start Scripting  l=l
+l=l 2 - Update Script    l=l
+l=l 3 - Exit Script      l=l
+\                          /
+ \                        /
+  \======================/
+""")
+op = input("\033[1;37m~$ ")
+
+if op == "1":
+    #Começando O 2° Script
+    os.system("clear")
+    print("\033[1;32m")
+    os.system("figlet Sloow-Xman")
+    print("""\033[1;33m
+    l=========================l\033[1;31m
+    l ~1 - Tool-X             l
+    l ~2 - Tool               l
+    l ~3 - Nmap               l
+    l ~4 - Inshackle          l
+    l ~5 - Cmatrix            l
+    l ~6 - sl                 l
+    l ~7 - Libaca             l
+    l ~8 - HeleScript         l
+    l ~9 - ZoroV1             l
+    l ~10 - F1GH              l
+    l ~11 - Color             l
+    l ~12 - CyberV1           l
+    l ~13 - infectV2          l
+    l ~14 - Looting           l
+    l ~15 - Dos               l
+    l ~16 - TitleV1           l
+    l ~17 - Zoro-PainelV1     l
+    l ~18 - Kali Linux        l
+    l ~17 - Red-Hawk          l
+    l ~18 - Update            l
+    l ~19 - Upgrade           l
+    l ~20 - Python3           l
+    l ~21 - Python2           l
+    l ~22 - Python            l
+    l ~23 - Git               l
+    l ~24 - Tor               l
+    l ~25 - Openssh           l
+    l ~26 - Php               l
+    l ~27 - Ruby              l
+    l ~28 - Proot             l
+    l ~29 - Cusos             l
+    l ~30 - Creditos          l
+    l ~31 - Sair              l\033[1;33m
+    l_________________________l
+    """)
+    op = input("~& ")
+    
+    if op == "1":
+        os.system("clear")
+        os.system("git clone https://github.com/Rajkumrdusad/Tool-X")
+        os.system("python3 install.py")
+    elif op == "2":
+        os.system("clear")
+        os.system("git clone https://github.com/noobfoda2/tool")
+        os.system("python3 install.py")
+    elif op == "3":
+        os.system("clear")
+        os.system("pkg install nmap")
+        os.system("python3 install.py")
+    elif op == "4":
+        os.system("clear")
+        os.system("git clone https://github.com/nandy121/inshackle")
+        os.system("python3 install.py")
+    elif op == "5":
+        os.system("clear")
+        os.system("pkg install cmatrix")
+        os.system("python3 install.py")
+    elif op == "6":
+        os.system("clear")
+        os.system("pkg install sl")
+        os.system("python3 install.py")
+    elif op == "7":
+        os.system("clear")
+        os.system("pkg install libaca")
+        os.system("python3 install.py")
+    elif op == "8":
+        os.systen("clear")
+        os.system("git clone https://github.com/Helexiel/HeleScript")
+        os.system("python3 install.py")
+    elif op == "9":
+        os.systen("clear")
+        os.system("git clone https://github.com/ZoroScripter/Zorov1.git")
+        os.system("python3 install.py")
+    elif op == "10":
+        os.systen("clear")
+        os.system("git clone https://github.com/ZoroScripter/F1GH.git")
+        os.system("python3 install.py")
+    elif op == "11":
+        os.system("clear")
+        os.system("git clone https://github.com/ZoroScripter/Color.git")
+        os.system("python3 install.py")
+    elif op == "12":
+        os.system("clear")
+        os.system("git clone https://github.com/ZoroScripter/Cyber-Punk.git")
+        os.system("python3 install.py")
+    elif op == "13":
+        os.system("clear")
+        os.system("git clone https://github.com/ZoroScripter/INFECTING.git")
+        os.system("python3 install.py")
+    elif op == "14":
+        os.system("clear")
+        os.system("git clone https://github.com/ZoroScripter/Looting.git")
+        os.system("python3 install.py")
+    elif op == "15":
+        os.system("clear")
+        os.system("git clone https://github.com/ZoroScripter/Looting.git")
+        os.system("python3 install.py")
+    elif op == "16":
+        os.system("clear")
+        os.system("git clone https://github.com/ZoroScripter/Titlev1.git")
+        os.system("python3 install.py")
+    elif op == "17":
+        os.system("clear")
+        os.system("git clone https://github.com/ZoroScripter/Zoro-Painel.git")
+        os.system("python3 install.py")
+    elif op == "18":
+        os.system("clear")
+        os.system("pkg update")
+        os.system("python3 install.py")
+    elif op == "19":
+        os.system("clear")
+        os.system("pkg upgrade")
+        os.system("python3 install.py")
+    elif op == "20":
+        os.system("clear")
+        os.systen("python3 -m install requests")
+        os.system("python3 install.py")
+    elif op == "21":
+        os.system("clear")
+        os.system("pkg install python2")
+        os.system("python3 install.py")
+    elif op == "22":
+        os.syatem("clear")
+        os.system("pkg install python")
+        os.system("python3 install.py")
+    elif op == "23":
+        os.system("clear")
+        os.system("pkg install git")
+        os.system("python3 install.py")
+    elif op == "24":
+        os.system("clear")
+        os.system("pkg install tor")
+        os.system("python3 install.py")
+    elif op == "25":
+        os.system("clear")
+        os.system("pkg install ssh")
+        os.system("python3 install.py")
+    elif op == "26":
+        os.system("clear")
+        os.system("pkg install php")
+        os.system("python3 install.py")
+    elif op == "27":
+        os.system("clear")
+        os.system("pkg install ruby")
+        os.system("python3 install.py")
+    elif op == "28":
+        os.system("clear")
+        os.system("pkg install proot")
+        os.system("python3 install.py")
+    elif op == "29":
+        os.system("clear")
+        print("""\033[1;35m
+        Bem...
+        Abaixo estão os melhores cursos de peogramação.
+        escolha um dos cursos e o scriot vai abrir o site
+        para vc fazer sua inscrção!!\033[1;32m
+        /=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\\
+        
+        1 - FullStackz                  l
+        2 - CodeWars                    l
+        3 - Java Na Web                 l
+        4 - Code Academy                l
+        5 - BitDegree                   l
+        \=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=/
+        """)
+        op = input("~! ")
+        
+        if op == "1":
+            os.system("xdg-open https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwic4tDrnf7xAhWKh5EKHW2oCdMYABABGgJjZQ&ae=2&dsh=1&agsac=jX0WFLLdhA0&cit=CkQKCAjwl_SHBhAbEjQA8Om1MtCrsl1AX0cZYjfHzw-C7fOW_xkf7XW5A8DSNxFu-W9hBgVxNsUnOCnxaZfKPp4ZGgI55_D_BwE&ohost=www.google.com&cid=CAESQOD2idjTuwUrgPD2oG8_kO1mBSCrKNDzuGplIDEGQwrsNmDnso4vVftQ5dbUzvJjPEHPqE44HTaNJS2v-0w6x6c&sig=AOD64_2K5lq4Lhp2mwXJKfbGpTdGWxrbeg&q&adurl&ved=2ahUKEwjE4srrnf7xAhUVr5UCHRXPDi8Q0Qx6BAgFEAE&ms=CoAC6ci1-9q_1DfFGmfKY6wHVLF0DlW4r6sT9kZ3wGGEeRgr1Ya0go8mkEhainWfUjBIXMgqhEFhu7vx1AioYreKHWLM1JAenc-JpeiT0TZf6w9VrR6lSZVcgs09Vge_2AUMUEdhW78HUVbwbgZuH-14azcdfFF3pDNb3mYXcombFx7p2SzUS-c7CPmSB2cyPxPwrWZ2bi9ZNTlkFhR2Gf6AbbRVfKN7wS1R36gmab6BVFPOsvQeiyuWnvTHNsGpZ2IqErJ_6yUKelDjyL0jUyAm30AAP7eAsKqAQxtpOFqkyK_Jbd-jpttB_NLQbDzRPGy_R8ba5ZH5ya1sF2XgohG-cAqAAhUBclRtDyaVxxtBfRS09C9WWf679cLUYncnFpyeqL8ManJZX06c28FXUWB2yumx_X_wQkmSQ_5luqckXWWhz4syd_fh4CzV8AJL9YNFCOxHRcuyf1xdduOaxMvyZZGt1MvHkNWEFoMEmeFDcxSV012408SgGlAYbKLY-Jxf5lbVQO8WRc0YssqHqzmFNzOxR8mwmXembsUthNvLyghHwJhQWszoA8GaTZhvsEHbKtK_1CTx9giWO5fU-sUKOsFZl4pyvY3Tf9_wqeaNhhaScr4-WjTTYKTQzHSNU6FDxgvEjl8QjElDxCh5hNWeGu_A3rmIY2rieq73VLzPlIH9veQSED_RWISM5nkaFHsUEWzIG3E")
+            os.system("python3 install.py")
+        elif op == "2":
+            os.system("xdg-open https://www.agit.com.br/?gclid=Cj0KCQjwl_SHBhCQARIsAFIFRVUcI1Ea6eSTtsRMijTKx7tampxhOThPBCcOtcKezrDGHrAo0hA6FsQaAjPSEALw_wcB")
+            os.system("python3 install.py")
+        elif op == "3":
+            os.system("xdg-open https://www.kasolution.com.br/oracle?pt=google&gclid=Cj0KCQjwl_SHBhCQARIsAFIFRVVt3YTydY5OX3qMzs_DYMUFUx6TrwMdtiPxPU6JjK52lKK51oInA3QaAuS1EALw_wcB")
+            os.system("python3 install.py")
+        elif op == "4":
+            os.system("xdg-open https://mentorama.com.br/python-developer/?roistat=google2_g_110172190033_514538738089_curso%20de%20python&roistat_referrer=&roistat_pos=&gclid=Cj0KCQjwl_SHBhCQARIsAFIFRVVutdknQOFd9RjDzsu6juCf3bDlestnmu8Ufq5l3ejXtwG7vVoioX8aAnJuEALw_wcB")
+            os.system("python3 install.py")
+    elif op == "30":
+        os.system("clear")
+        print("\033[1;32m Criador:")
+        time.sleep(1)
+        print("ZoroScripter")
+        time.sleep(1)
+        
+        print("Ajudantes:")
+        time.sleep(1)
+        print("W4li")
+        time.sleep(1)
+        print("Columbiaxxs")
+        time.sleep(4)
+        os.system("python3 install.py")
+    elif op == "31":
+        print("Saindoo...")
+        time.sleep(4)
+        exit()
+elif op == "2":
+    os.system("python3 install.py")
+elif op == "3":
+    print("Saindo...")
+    time.sleep(3)
+    os.system("cd")
+    exit()
+else:
+    os.system("python3 install.py")
